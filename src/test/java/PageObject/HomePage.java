f1fb7e8156870cfd189a7f8e07bc800ef93188a2
shruti <shrutikhungar@gmail.com>
2018-04-02 10:43:57 +0530

    checked and pushing to shruti

--- src/test/java/PageObject/HomePage.java
+++ src/test/java/PageObject/HomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import config.Configuration;
 
 public class HomePage {
+	/*all objects belonging to Home page*/
 	WebDriver driver=Configuration.driver;
 	By loginLink=By.xpath("//label[@for='c1']");
 	
